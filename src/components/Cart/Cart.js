087e3c6cb1a8b8e2b121a175e5ef5547d916c2b8
Wolferner <m.wolfern@yandex.com>
2023-11-20 16:56:00 +0200

    created Modal Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import styles from './Cart.module.css'
+import Modal from '../UI/Modal'
 
 const Cart = (props)=>{
 
@@ -8,7 +9,7 @@ const Cart = (props)=>{
         </ul>)
 
     return(
-        <div>
+        <Modal>
             {cartItems}
             <div className={styles.total}>
                 <span>Summ</span>
@@ -18,7 +19,7 @@ const Cart = (props)=>{
                 <button className={styles['button--alt']}>Close</button>
                 <button className={styles.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     )
 }
 
