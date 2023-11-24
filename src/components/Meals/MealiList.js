046a66776e24cb4c310c6e5ecd804e3d1d61ea4a
Wolferner <m.wolfern@yandex.com>
2023-11-24 14:09:54 +0200

    Join test db

--- src/components/Meals/MealiList.js
+++ src/components/Meals/MealiList.js
@@ -1,39 +1,50 @@
+import { useEffect, useState} from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import styles from './MealList.module.css'
 
-const DUMMY_MEALS = [
-    {
-      id: "m1",
-      name: 'Ролл "Наоми"',
-      description:
-        "Сыр Филадельфия, куриное филе, масаго, помидор, огурец, кунжут",
-      price: 11.99,
-    },
-    {
-      id: "m2",
-      name: "Спайс в лососе",
-      description: "Рис, лосось, соус спайс",
-      price: 3.99,
-    },
-    {
-      id: "m3",
-      name: "Суши с угрем",
-      description: "Угорь копченый, соус унаги, кунжут",
-      price: 4.99,
-    },
-    {
-      id: "m4",
-      name: 'Салат "Поке с лососем"',
-      description:
-        "Рис, лосось, огурец, чука, нори, стружка тунца, соус ореховый",
-      price: 7.99,
-    },
-  ];
 
 const MealList =()=>{
+  const [dummyMeals, setDummyMeals] = useState([])
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState('')
 
-    const mealList = DUMMY_MEALS.map((meal)=>
+  const fetchDummyMeals = async () =>{
+    setError(null)
+    setIsLoading(true)
+    try{
+      const response = await fetch('https://react-course-project-ffeb9-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+      if(!response.ok){
+        throw new Error('Some problemka')
+      }
+
+      const data = await response.json()
+      const loadedMeals=[]
+      for( const key in data){
+        loadedMeals.push({
+          id:key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+      setDummyMeals(loadedMeals)
+
+    }catch(e){
+      setError(e.message)
+    }
+
+    if(error){
+      console.log(error)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchDummyMeals()
+  },[])
+
+    const mealList = dummyMeals.map((meal)=>
     <MealItem 
       name={meal.name} 
       description = {meal.description}
@@ -41,6 +52,16 @@ const MealList =()=>{
       key={meal.id}
       id={meal.id}/>
     )
+
+
+    
+    if(isLoading){
+      return(
+        <section className={styles.loading}>
+          <p>IDJOT ZAGRUZKA....</p>
+        </section>
+      )
+    }
 
     return(
         <section className={styles.meals}>
